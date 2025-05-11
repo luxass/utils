05b6145fd53612131d7d2352fb553d80280ab94d
Lucas <lucasnrgaard@gmail.com>
2025-05-11 09:17:59 +0200

    chore: add JSDoc comments for `invariant` function to improve documentation

--- src/common.ts
+++ src/common.ts
@@ -7,6 +7,15 @@ export class InvariantError extends Error {
   }
 }
 
+/**
+ * Asserts that a condition is truthy. If the condition is falsy, throws an InvariantError.
+ * This function is useful for ensuring invariants and preconditions in code.
+ *
+ * @param {unknown} predicate - The condition to check
+ * @param {string} message - Error message to display if the condition fails
+ * @param {unknown[]} positionals - Values to substitute into the error message using formatStr
+ * @throws {InvariantError} Throws when the predicate is falsy
+ */
 export function invariant(
   predicate: unknown,
   message: string,
